f901ba709f8bf421ba524c34e92ff342c476625a
Nazarene23 <iamnashferia@yahoo.com>
2026-08-22 15:00:01 +0800

    Add Solace presence status

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,22 +1,38 @@
+const {
+  ActivityType,
+} = require('discord.js');
+
 const logger = require(
-    '../utils/logger',
-  );
-  
-  const {
-    startScheduler,
-  } = require(
-    '../scheduler/scheduler',
-  );
-  
-  module.exports = {
-    name: 'clientReady',
-    once: true,
-  
-    execute(client) {
-      logger.success(
-        `${client.user.tag} is now online.`,
-      );
-  
-      startScheduler(client);
-    },
-  };
+  '../utils/logger',
+);
+
+const {
+  startScheduler,
+} = require(
+  '../scheduler/scheduler',
+);
+
+module.exports = {
+  name: 'clientReady',
+  once: true,
+
+  execute(client) {
+    client.user.setPresence({
+      activities: [
+        {
+          name: 'Solace',
+          type: ActivityType.Custom,
+          state: 'Supporting your wellbeing 🌿',
+        },
+      ],
+
+      status: 'online',
+    });
+
+    logger.success(
+      `${client.user.tag} is now online.`,
+    );
+
+    startScheduler(client);
+  },
+};
